ed935e9d25fc8c80144c984d9113e851fb1117d4
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-03-08 20:34:59 +0530

    Group creation function added

---
+++ lib/Controller/GroupController.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:sampark/Controller/ProfileController.dart';
+import 'package:sampark/Model/GroupModel.dart';
+import 'package:sampark/Model/UserMode.dart';
+import 'package:sampark/Pages/Home/HomePage.dart';
+import 'package:uuid/uuid.dart';
+
+class GroupController extends GetxController {
+  RxList<UserModel> groupMembers = <UserModel>[].obs;
+  final db = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
+  var uuid = Uuid();
+  RxBool isLoading = false.obs;
+  ProfileController profileController = Get.put(ProfileController());
+  void selectMember(UserModel user) {
+    if (groupMembers.contains(user)) {
+      groupMembers.remove(user);
+    } else {
+      groupMembers.add(user);
+    }
+  }
+
+  Future<void> createGroup(String groupName, String imagePath) async {
+    isLoading.value = true;
+    String groupId = uuid.v6();
+
+    try {
+      String imageUrl = await profileController.uploadFileToFirebase(imagePath);
+
+      await db.collection("groups").doc(groupId).set(
+        {
+          "id": groupId,
+          "name": groupName,
+          "profileUrl": imageUrl,
+          "members": groupMembers.map((e) => e.toJson()).toList(),
+          "createdAt": DateTime.now().toString(),
+          "createdBy": auth.currentUser!.uid,
+          "timeStamp": DateTime.now().toString(),
+        },
+      );
+      // Group Created tost
+      Get.snackbar("Group Created", "Group Created Successfully");
+      Get.offAll(HomePage());
+      isLoading.value = false;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/Pages/Groups/NewGroup/NewGroup.dart
+++ lib/Pages/Groups/NewGroup/NewGroup.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sampark/Controller/ContactController.dart';
+import 'package:sampark/Controller/GroupController.dart';
+import 'package:sampark/Pages/Groups/NewGroup/GroupTitle.dart';
+import 'package:sampark/Pages/Groups/NewGroup/SelectedMemberList.dart';
 import 'package:sampark/Pages/Home/Widget/ChatTile.dart';
-
 import '../../../Config/Images.dart';
 
 class NewGroup extends StatelessWidget {
@@ -11,48 +13,87 @@ class NewGroup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ContactController contactController = Get.put(ContactController());
+    GroupController groupController = Get.put(GroupController());
     return Scaffold(
       appBar: AppBar(
         title: Text('New Group'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.arrow_forward),
+      floatingActionButton: Obx(
+        () => FloatingActionButton(
+          backgroundColor: groupController.groupMembers.isEmpty
+              ? Colors.grey
+              : Theme.of(context).colorScheme.primary,
+          onPressed: () {
+            if (groupController.groupMembers.isEmpty) {
+              Get.snackbar("Error", "Please select atleast one member");
+            } else {
+              Get.to(GroupTitle());
+            }
+          },
+          child: Icon(
+            Icons.arrow_forward,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: StreamBuilder(
-          stream: contactController.getContacts(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text("Error: ${snapshot.error}"),
-              );
-            }
-            if (snapshot.data == null) {
-              return const Center(
-                child: Text("No Messages"),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return ChatTile(
-                    imageUrl: snapshot.data![index].profileImage ??
-                        AssetsImage.defaultProfileUrl,
-                    name: snapshot.data![index].name!,
-                    lastChat: snapshot.data![index].about ?? "",
-                    lastTime: "",
-                  );
+        child: Column(
+          children: [
+            SelectedMembers(),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Text(
+                  "Contacts on Sampark",
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: StreamBuilder(
+                stream: contactController.getContacts(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    );
+                  }
+                  if (snapshot.data == null) {
+                    return const Center(
+                      child: Text("No Messages"),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () {
+                            groupController.selectMember(snapshot.data![index]);
+                          },
+                          child: ChatTile(
+                            imageUrl: snapshot.data![index].profileImage ??
+                                AssetsImage.defaultProfileUrl,
+                            name: snapshot.data![index].name!,
+                            lastChat: snapshot.data![index].about ?? "",
+                            lastTime: "",
+                          ),
+                        );
+                      },
+                    );
+                  }
                 },
-              );
-            }
-          },
+              ),
+            ),
+          ],
         ),
       ),
     );
